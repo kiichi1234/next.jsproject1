0b3f31b95df480caa8fd7e3c6bc84708e2a836ad
kiichi1234 <second02kiy@gmail.com>
2025-07-26 01:46:41 +0900

    フロントユーザー登録成功

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { jwtVerify } from "jose";
 
 export async function middleware(request) {
-    const token="eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImR1bW15MkBnbWFpbC5jb20iLCJleHAiOjE3NTI4MzAyNjd9.Tii-07BdJZW197XLFXJtRbmIgfwjDLx8sAzVTCNn1pc"
+    const token="eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImR1bW15QGdtYWlsLmNvbSIsImV4cCI6MTc1MzA3MjMxNH0.IuDZtNqXHU0hr0ZYvSceQrmpZke5mQfh-JnLDXvyAk4"
     //await request.headers.get("Authorization")?.split("")[1]
 
     if(!token){
