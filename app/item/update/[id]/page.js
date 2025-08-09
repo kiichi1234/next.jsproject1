1d5c4fd516f724ce5a416278d30545a3859387ca
kiichi1234 <second02kiy@gmail.com>
2025-08-09 16:39:42 +0900

    表示制限削除まで完了

--- app/item/update/[id]/page.js
+++ app/item/update/[id]/page.js
@@ -1,24 +1,40 @@
 "use client"
-import { useState } from "react"
+import { useState,useEffect } from "react"
 import { useRouter } from "next/navigation"
-import useAuth from "../../utils/useAuth"
+import useAuth from "../../../utils/useAuth"
 
-const CreateItem= ()=>{
+
+const UpdateItem= (context)=>{
     
     
     const [title,setTitle] = useState("")
     const [price,setPrice] = useState("")
     const [image,setImage] = useState("")
     const [description,setDescription] = useState("")
+    const [email,setEmail] = useState("")
     const router= useRouter()
-    const loginUserEmail = useAuth()
-    console.log(loginUserEmail)
+    const loginUserEmail =useAuth()
+
+    useEffect(() => {
+     const getSingleItem= async(id)=>{
+     const responce = await fetch(`http://localhost:3000/api/item/readsingle/${id}`,{cache:"no-store"})
+     const jsonData = await responce.json()
+     const singleItem = jsonData.singleItem
+     
+     setTitle(singleItem.title)
+     setPrice(singleItem.price)
+     setImage(singleItem.image)
+     setDescription(singleItem.description)
+     setEmail(singleItem.email)
+    }
+    getSingleItem(context.params.id)
+},[context] )
 
     const handleSubmit=async(e)=>{
         e.preventDefault()
         try{
-            const response= await fetch("http://localhost:3000/api/item/create",{
-                method:"POST",
+            const response= await fetch('http://localhost:3000/api/item/update/${context.params.id}',{
+                method:"PUT",
                 headers:{
                     "Accept":"application/json",
                     "Content-Type":"application/json",
@@ -38,14 +54,14 @@ const CreateItem= ()=>{
             router.push("/")
             router.refresh()
         }catch{
-            alert("アイテム作成失敗")
+            alert("アイテム編集失敗")
         }
     }
 
-    if(loginUserEmail){
+    if(loginUserEmail===email){
         return(
             <div>
-                <h1>アイテム作成</h1>
+                <h1>アイテム編集</h1>
                 <form onSubmit={handleSubmit}>
                     <input value ={title} onChange={(e)=>setTitle(e.target.value)}
                     type="text" name="title" placeholder="アイテム名" required/>
@@ -55,10 +71,12 @@ const CreateItem= ()=>{
                     type="text" name="image" placeholder="画像" required/>
                     <textarea value ={description} onChange={(e)=>setDescription(e.target.value)}
                     name="description" rows={15}  placeholder="商品説明" required></textarea>
-                    <button>作成</button>
+                    <button>編集</button>
                 </form>
             </div>
         )
+    }else{
+        return<h1>権限がありません</h1>
     }
 }
-export default CreateItem
+export default UpdateItem
